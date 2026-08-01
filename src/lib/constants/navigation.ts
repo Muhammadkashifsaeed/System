6c89fe073cebcb17a1ca8b9e146d5f0bfb7525ab
Kashif <kashifsaeed8856@gmail.com>
2026-08-01 18:37:42 +0500

    style: change Industries Served nav label to Industries

--- src/lib/constants/navigation.ts
+++ src/lib/constants/navigation.ts
@@ -12,7 +12,7 @@ export interface NavItem {
 
 export const topNavigation: NavItem[] = [
   { label: "HOME", href: "/" },
-  { label: "INDUSTRIES SERVED", href: "/industries-served" },
+  { label: "INDUSTRIES", href: "/industries-served" },
   {
     label: "ABOUT",
     href: "/about",
